581b8bfa4f38764006ecc0bd0b212a93718ec93c
Bence Borbely (ED/ESY-Bp) <Bence.Borbely@hu.bosch.com>
2017-06-14 20:10:12 +0200

    Dynamically extendible doubly linked list is added

--- 13/dllist.c
+++ 13/dllist.c
@@ -1,9 +1,10 @@
-#include <dllist.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "dllist.h"
 
+/* TODO: need a workaround for freeing up data in nodes */
 
-static struct dllist_node* dll_node_create(void* data)
+static struct dllist_node* dllist_node_create(void* data)
 {
   struct dllist_node* n = (struct dllist_node*)malloc(sizeof(struct dllist_node));
   n->data = data;
@@ -12,10 +13,10 @@ static struct dllist_node* dll_node_create(void* data)
   return n;
 }
 
-void dllist_push_back(struct dllist* dll, void * data)
+void dllist_push_back(struct dllist* dll, void* data)
 {
   struct dllist_node* n;
-  n = dll_node_create(data);
+  n = dllist_node_create(data);
   n->previous = dll->end;
   if (!dll->begin) {
     dll->begin = n;
@@ -27,15 +28,16 @@ void dllist_push_back(struct dllist* dll, void * data)
   ++dll->length;
 }
 
-void dllist_push_front(struct dllist* dll, void * data)
+void dllist_push_front(struct dllist* dll, void* data)
 {
   struct dllist_node* n;
-  n = dll_node_create(data);
+  n = dllist_node_create(data);
   if (!dll->begin) {
     dll->begin = n;
     dll->end = n;
   } else { 
     n->next = dll->begin;
+    dll->begin->previous = n;
     dll->begin = n;
   }
   ++dll->length;
@@ -47,13 +49,13 @@ void dllist_pop_back(struct dllist* dll)
   if (!dll->end) { 
     return;
   } else if (dll->begin == dll->end) {
-    free(dll->end->data);
+    /*free(dll->end->data);*/
     free(dll->end);
     dll->begin = NULL;
     dll->end = NULL;
   } else {
     tmp = dll->end->previous;
-    free(dll->end->data);
+    /*free(dll->end->data);*/
     free(dll->end);
     tmp->next = NULL;
     dll->end = tmp;
@@ -67,15 +69,16 @@ void dllist_pop_front(struct dllist* dll)
   if (!dll->begin) { 
     return;
   } else if (dll->begin == dll->end) {
-    free(dll->begin->data);
+    /*free(dll->begin->data);*/
     free(dll->begin);
     dll->begin = NULL;
     dll->end = NULL;
   } else {
     tmp = dll->begin->next;
-    free(dll->begin->data);
+    /*free(dll->begin->data);*/
     free(dll->begin);
     dll->begin = tmp;
+    dll->begin->previous = NULL;
   }
   --dll->length;
 }
@@ -94,26 +97,100 @@ void dllist_insert(struct dllist* dll, int idx, void* data)
     dllist_push_back(dll, data);
   } else {
     /* TODO: use dllist_idx here */
-    for (i = 0; i < idx; ++i) tmp = tmp->next;
-    n = dll_node_create(data);
+    /*for (i = 0; i < idx; ++i) tmp = tmp->next;*/
+    tmp = dllist_idx(dll, idx);
+    n = dllist_node_create(data);
     n->next = tmp->next;
     n->previous = tmp;
     tmp->next = n;
     n->next->previous = n;
+    ++dll->length;
   }
 }
 
 struct dllist_node* dllist_idx(struct dllist* dll, int idx)
 {
   int i;
-  struct dllist_node* l;
+  struct dllist_node* l = dll->begin;
   if (dll->length <= idx) return NULL;
   for (i = 0; i < idx; ++i) l = l->next;
-  return l->next;
+  return l;
 }
 
-void dllist_remove(struct dllist*, idx);
-int dllist_find(struct dllist*, int*, void*); 
-void dllist_reverse(struct dllist*);
-void dllist_free(struct dllist*);
+void dllist_remove(struct dllist* dll, int idx)
+{
+  struct dllist_node* p;
+  if (idx >= dll->length) {
+    return;
+  } else if (idx == 0) { 
+    dllist_pop_front(dll);
+  } else if (idx == (dll->length - 1)) {
+    dllist_pop_back(dll);
+  } else {
+    p = dllist_idx(dll, idx);
+    p->previous->next = p->next;
+    p->next->previous = p->previous;
+    /*free(p->data);*/
+    free(p);
+    --dll->length;
+  }
+}
+
+int dllist_find(struct dllist* dll, int** matchlist, void* data, int size)
+{ 
+  struct dllist_node* n = dll->begin;
+  int i, j = 0, k = 0, *m = (int*)malloc(dll->length * sizeof(int));
+  while (n) {
+    for (i = 0; i < size; ++i) {
+      if (!n->data && !data) 
+        continue;
+      else if (!data || !n->data || ((char*)n->data)[i] != ((char*)data)[i]) 
+        break;
+    }
+    if (i == size) m[j++] = k;
+    n = n->next;
+    ++k;
+  }
+  *matchlist = (int*)malloc(k * sizeof(int));
+  for (i = 0; i < j; ++i) (*matchlist)[i] = m[i];
+  free(m);
+  return j;
+}
+
+void dllist_reverse(struct dllist* dll)
+{ 
+  struct dllist_node* n = dll->end, *tmp;
+  while (n) {
+    tmp = n->previous;
+    n->previous = n->next;
+    n->next = tmp;
+    n = n->next;
+  } 
+  tmp = dll->begin;
+  dll->begin = dll->end; 
+  dll->end = tmp;
+}
+
+void dllist_free(struct dllist* dll)
+{ 
+  struct dllist_node* n = dll->begin;
+  while (n->next) {
+    n = n->next;
+    /*free(n->previous->data);*/
+    free(n->previous);
+  }
+  /*free(n->data); */
+  free(n); 
+  dll->begin = NULL;
+  dll->end = NULL;
+}
+
+void dllist_print(struct dllist* dll, void (*node_writer)(void*))
+{ 
+  struct dllist_node* n = dll->begin;
+  while (n) {
+    (*node_writer)(n->data); 
+    n = n->next;
+  }
+}
 
--- 13/dllist.h
+++ 13/dllist.h
@@ -2,26 +2,54 @@
 #define LISTH
 
 struct dllist {
-  struct ll_node* begin;
-  struct ll_node* end;
-  int             length;
+  struct dllist_node* begin;
+  struct dllist_node* end;
+  int                 length;
 };
 
 struct dllist_node {
-  struct dll_node* next;
-  struct dll_node* previous; 
-  void*            data;
+  struct dllist_node* next;
+  struct dllist_node* previous; 
+  void*               data;
 };
 
-void dllist_push_back(struct dllist*, void *);
-void dllist_push_front(struct dllist*, void *);
+/* push to the end of the list                                                */
+void dllist_push_back(struct dllist*, void*);
+
+/* pus to the front of the list                                               */
+void dllist_push_front(struct dllist*, void*);
+
+/* pop from the end of the list                                               */
 void dllist_pop_back(struct dllist*);
+
+/* pop from the beginning of the list                                         */
 void dllist_pop_front(struct dllist*);
-void dllist_insert(struct dllist*, int);
+
+/* insert to a specified index of the list                                    */
+void dllist_insert(struct dllist*, int, void*);
+
+/* get pointer of the node in the list at a specified positio, returns NULL   */
+/* if idx is outside the range of the list                                    */ 
 struct dllist_node* dllist_idx(struct dllist*, int);
-void dllist_remove(struct dllist*, idx);
-int dllist_find(struct dllist*, int*, void*); 
+
+/* removes node from the list from a specified position                       */ 
+void dllist_remove(struct dllist*, int);
+
+/* collect indices of nodes in the list of which the data field equals to the */ 
+/* specified data (with specified size in the last argument), returns the     */ 
+/* number of elements found                                                   */ 
+/* can find empty elements (NULL should be passed as data pointer in this     */ 
+/* case) resulting from dynamic extension of the list                         */ 
+int dllist_find(struct dllist*, int**, void*, int);
+
+/* reverse the order of the list                                              */ 
 void dllist_reverse(struct dllist*);
+
+/* free the whole list (except for the dllist structure)                      */ 
 void dllist_free(struct dllist*);
+
+/* prints the contnets to the standard input using the passed function for    */ 
+/* writing the data of nodes                                                  */ 
+void dllist_print(struct dllist*, void (*)(void*));
 
 #endif
